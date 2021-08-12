b9fbf6ca4082e7ad4b08f92418f70ad075622303
awongh <akira@awongh.com>
2021-08-12 21:50:31 +0800

    browser compatibility logic, add course name

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
-var searchParams = new URLSearchParams(window.location.search);
+if( "URLSearchParams" in window ){
 
-if( searchParams.has("name") && searchParams.has("date") && searchParams.has("hash") ){
-  var name = searchParams.get("name");
-  var date = searchParams.get("date");
-  var hash = searchParams.get("hash");
+  var searchParams = new window.URLSearchParams(window.location.search);
 
-  document.querySelector('#rocket-cert-name').innerText = atob(hash);
-  document.querySelector('#rocket-cert-date').innerText = date;
+  if( searchParams.has("name") && searchParams.has("date") && searchParams.has("hash") ){
+    var name = searchParams.get("name");
+    var date = searchParams.get("date");
+    var hash = searchParams.get("hash");
+    var courseName = searchParams.get("courseName") || 'Coding Basics';
+
+    document.querySelector('#rocket-cert-name').innerText = atob(hash);
+    document.querySelector('#rocket-cert-date').innerText = date;
+    document.querySelector('#course-name').innerText = courseName;
+  }else{
+    window.location = "https://rocketacademy.co";
+  }
 }else{
-  //window.location = "https://rocketacademy.co";
+  alert('Sorry your browser is too out of date to view this page.');
 }
